c195a02e9ef88abbb5bc24bfc3d1b03b36a730f6
Yuhanncito <uchijaisuka02@gmail.com>
2024-03-23 20:34:05 -0600

    secretPassword

--- src/components/ForgetSecret.jsx
+++ src/components/ForgetSecret.jsx
@@ -8,8 +8,10 @@ import Cookies from 'universal-cookie';
 
 function ForgetSecret() {
 
-  const  {generalData} = useUserContext();
+  const  {generalData, setGeneralData} = useUserContext();
   const cookie = new Cookies();
+
+  const [quest, setQuest] = useState([])
 
     const handleChange = e =>{
       setForm({
@@ -20,7 +22,7 @@ function ForgetSecret() {
 
     const [form, setForm] = useState({
       email:generalData.email,
-      secret: '',
+      secret: generalData.questionKey[0].key,
       respuestaSecreta: ''
     })
 
@@ -50,9 +52,16 @@ function ForgetSecret() {
 
           return
         }
-
-        cookie.set('x-access-user', json.token, { path: '/', expires: new Date(Date.now() + 86400000) } );
-        navigate('/App')
+        const response = await fetch(CONFIGURACIONES.BASEURL+"/auth/forgotPassword",{
+          method:"POST",
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body:JSON.stringify(form)
+        });
+        setGeneralData({option:'forgot',email:form.email})
+        navigate("/CodigoVer")
+        console.log(json)
         
       } catch (error) {
         Swal.fire({
@@ -62,45 +71,19 @@ function ForgetSecret() {
         });
       }
     }
-
-
-    useEffect(() => {
-      
-      const getData = async() =>{
-        try {
-          const res = await fetch(CONFIGURACIONES.BASEURL+`/auth/${generalData.email}`,{
-            headers:{
-              'Content-Type': 'application/json',
-            }
-          })
-          const json = await res.json();
-         
-          if(json.message!=='ok'){
-            navigate('/ForgetPass')
-          }
-        } catch (error) {
-          console.log(error)
-        }
-      }
-      getData()
-    }, [])
+    
+    
     
 
   return (
     <div className="flex justify-center h-screen w-screen items-center">
     <form onSubmit={handleSubmit(onSubmit)} className="flex bg-white flex-col w-[90%] shadow-xl rounded-2xl lg:w-[50%] h-[60%] px-10 items-center justify-center">
         <h1 className="md:text-5xl text-4xl font-semibold text-center">Recuperación de Contraseña</h1>
-        <h1 className="md:text-5xl text-4xl font-semibold text-center">Usuario {generalData.email}</h1>
+        <h1 className="md:text-5xl text-4xl font-semibold text-center">Usuario {generalData.name}</h1>
         <div className='w-full flex flex-col'>
             <label htmlFor="secret" className="text-lg font-medium">Pregunta secreta</label>
-              <select {...register('secret', { required: true, validate: value => value !== "default" })} name="secret" id="secret" className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" onChange={handleChange}>
-                <option value="default">Selecciona tu pregunta</option>
-                <option value="colorFavorito">¿Cuál es tu color favorito?</option>
-                <option value="nombreMascota">¿Cómo se llama tu primera mascota?</option>
-                <option value="ciudadNacimiento">¿En qué ciudad naciste?</option>
-                <option value="comidaFavorita">¿Cuál es tu comida favorita?</option>
-              </select>
-              {errors.secret && <p className="text-red-500 text-xs italic text-center">Por favor, selecciona una pregunta.</p>}
+              <input type="text" name="questionName" className="text-lg font-medium" id="questionName" value={generalData.questionKey[0].question} disabled/>
+              <input type="hidden" name="questionKey" value={generalData.questionKey[0].key}/>
               <label htmlFor="respuestaSecreta" className="text-lg font-medium">Respuesta</label>
               <input {...register('respuestaSecreta', { required: true })} type="text" name="respuestaSecreta" id="respuestaSecreta" className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" onChange={handleChange}/>
               {errors.respuestaSecreta && <p className="text-red-500 text-xs italic text-center">Este campo es requerido.</p>}
