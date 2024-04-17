4a3d592e03971870d6ac12d3ceee2f4ddcf7a2c8
Yuhanncito <127004490+Yuhanncito@users.noreply.github.com>
2024-04-17 01:16:47 -0600

    Update confing.jsx

--- src/configs/confing.jsx
+++ src/configs/confing.jsx
@@ -1,4 +1,4 @@
 export const CONFIGURACIONES={
-    BASEURL0:'https://proto-api2-0.vercel.app/api',
-    BASEURL:'http://localhost:4000/api'
+    BASEURL:'https://proto-api2-0.vercel.app/api',
+    BASEURL0:'http://localhost:4000/api'
 }
